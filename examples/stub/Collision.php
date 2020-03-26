1809edef59aa4c81d66a974afa3d58c82338459f
Joseph Montanez <sutabi@gmail.com>
2020-03-25 20:29:48 -0700

    Added other collision calls

--- examples/stub/Collision.php
+++ examples/stub/Collision.php
@@ -5,15 +5,73 @@ namespace raylib;
 
 class Collision
 {
+
+//    // Basic shapes collision detection functions
+//    RLAPI bool CheckCollisionRecs(Rectangle rec1, Rectangle rec2);                                           // Check collision between two rectangles
+//    RLAPI bool CheckCollisionCircles(Vector2 center1, float radius1, Vector2 center2, float radius2);        // Check collision between two circles
+//    RLAPI bool CheckCollisionCircleRec(Vector2 center, float radius, Rectangle rec);                         // Check collision between circle and rectangle
+//    RLAPI Rectangle GetCollisionRec(Rectangle rec1, Rectangle rec2);                                         // Get collision rectangle for two rectangles collision
+//    RLAPI bool CheckCollisionPointRec(Vector2 point, Rectangle rec);                                         // Check if point is inside rectangle
+//    RLAPI bool CheckCollisionPointCircle(Vector2 point, Vector2 center, float radius);                       // Check if point is inside circle
+//    RLAPI bool CheckCollisionPointTriangle(Vector2 point, Vector2 p1, Vector2 p2, Vector2 p3);               // Check if point is inside a triangle
+
+
     /**
-     * Check if point is inside rectangle
+     * Check collision between two rectangles
      *
-     * @param array $point The point to check - Vector2 array ['x' => 0, 'y' => 0]
-     * @param array $rec   The rectangle to check - Rectangle array ['x' => 0, 'y' => 0, 'width' => 0, 'height' => 0]
+     * @param \raylib\Rectangle $rec1 The rectangle to check
+     * @param \raylib\Rectangle $rec2 The rectangle to check
      *
-     * {@internal bool CheckCollisionPointRec(Vector2 point, Rectangle rec) }}
+     * {@internal RLAPI bool CheckCollisionRecs(Rectangle rec1, Rectangle rec2); }}
      *
      * @return bool
      */
-    public static function checkPointRec(array $point, array $rec) : bool {}
+    public static function checkRecs(Rectangle $rec1, Rectangle $rec2): bool
+    {
+    }
+
+    /**
+     * Check collision between two circles
+     *
+     * @param \raylib\Vector2 $center1
+     * @param float   $radius1
+     * @param \raylib\Vector2 $center2
+     * @param float   $radius2
+     *
+     * {@internal RLAPI bool CheckCollisionCircles(Vector2 center1, float radius1, Vector2 center2, float radius2); }}
+     *
+     * @return bool
+     */
+    public static function checkCircles(Vector2 $center1, float $radius1, Vector2 $center2, float $radius2): bool
+    {
+    }
+
+    /**
+     * Check collision between circle and rectangle
+     *
+     * {@internal RLAPI bool CheckCollisionCircleRec(Vector2 center, float radius, Rectangle rec); }}
+     *
+     * @param \raylib\Vector2   $center
+     * @param float             $radius
+     * @param \raylib\Rectangle $rec
+     *
+     * @return bool
+     */
+    public static function checkCircleRec(Vector2 $center, float $radius, Rectangle $rec): bool
+    {
+    }
+
+    /**
+     * Check if point is inside rectangle
+     *
+     * @param array $point The point to check
+     * @param array $rec   The rectangle to check
+     *
+     * {@internal RLAPI bool CheckCollisionPointRec(Vector2 point, Rectangle rec) }}
+     *
+     * @return bool
+     */
+    public static function checkPointRec(Vector2 $point, Rectangle $rec): bool
+    {
+    }
 }
--- raylib-collision.c
+++ raylib-collision.c
@@ -87,7 +87,70 @@ PHP_METHOD(Collision, __construct)
     php_raylib_collision_object *intern = Z_COLLISION_OBJ_P(getThis());
 }
 
-//bool CheckCollisionPointRec(Vector2 point, Rectangle rec)
+
+// Check collision between two rectangles
+// RLAPI bool CheckCollisionRecs(Rectangle rec1, Rectangle rec2);
+PHP_METHOD(Collision, checkRecs)
+{
+    zval *rec1;
+    zval *rec2;
+
+    ZEND_PARSE_PARAMETERS_START(2, 2)
+        Z_PARAM_ZVAL(rec1)
+        Z_PARAM_ZVAL(rec2)
+    ZEND_PARSE_PARAMETERS_END();
+
+    php_raylib_rectangle_object *phpRect1 = Z_RECTANGLE_OBJ_P(rec1);
+    php_raylib_rectangle_object *phpRect2 = Z_RECTANGLE_OBJ_P(rec2);
+
+    RETURN_BOOL(CheckCollisionRecs(phpRect1->rectangle, phpRect2->rectangle));
+}
+
+// Check collision between two circles
+// RLAPI bool CheckCollisionCircles(Vector2 center1, float radius1, Vector2 center2, float radius2);
+PHP_METHOD(Collision, checkCircles)
+{
+    zval *center1;
+    double radius1;
+    zval *center2;
+    double radius2;
+
+    ZEND_PARSE_PARAMETERS_START(4, 4)
+        Z_PARAM_ZVAL(center1)
+        Z_PARAM_DOUBLE(radius1)
+        Z_PARAM_ZVAL(center2)
+        Z_PARAM_DOUBLE(radius2)
+    ZEND_PARSE_PARAMETERS_END();
+
+    php_raylib_vector2_object *phpCenter1 = Z_VECTOR2_OBJ_P(center1);
+    php_raylib_vector2_object *phpCenter2 = Z_VECTOR2_OBJ_P(center2);
+
+    RETURN_BOOL(CheckCollisionCircles(phpCenter1->vector2, (float) radius1, phpCenter2->vector2, (float) radius2));
+}
+
+// Check collision between circle and rectangle
+// RLAPI bool CheckCollisionCircleRec(Vector2 center, float radius, Rectangle rec);
+PHP_METHOD(Collision, checkCircleRec)
+{
+    zval *center;
+    double radius;
+    zval *rec;
+
+    ZEND_PARSE_PARAMETERS_START(3, 3)
+        Z_PARAM_ZVAL(center)
+        Z_PARAM_DOUBLE(radius)
+        Z_PARAM_ZVAL(rec)
+    ZEND_PARSE_PARAMETERS_END();
+
+    php_raylib_vector2_object *phpCenter = Z_VECTOR2_OBJ_P(center);
+    php_raylib_rectangle_object *phpRec = Z_RECTANGLE_OBJ_P(rec);
+
+    RETURN_BOOL(CheckCollisionCircleRec(phpCenter->vector2, (float) radius, phpRec->rectangle));
+}
+
+
+// Check if point is inside rectangle
+// RLAPI bool CheckCollisionPointRec(Vector2 point, Rectangle rec);
 PHP_METHOD(Collision, checkPointRec)
 {
     zval *point;
@@ -104,10 +167,87 @@ PHP_METHOD(Collision, checkPointRec)
     RETURN_BOOL(CheckCollisionPointRec(phpPoint->vector2, phpRect->rectangle));
 }
 
+// Get collision rectangle for two rectangles collision
+PHP_METHOD(Collision, getRec)
+{
+    zval *rec1;
+    zval *rec2;
+
+    ZEND_PARSE_PARAMETERS_START(2, 2)
+        Z_PARAM_ZVAL(rec1)
+        Z_PARAM_ZVAL(rec2)
+    ZEND_PARSE_PARAMETERS_END();
+
+    php_raylib_rectangle_object *phpRec1 = Z_RECTANGLE_OBJ_P(rec1);
+    php_raylib_rectangle_object *phpRec2 = Z_RECTANGLE_OBJ_P(rec2);
+
+    zval *obj = malloc(sizeof(zval));
+    object_init_ex(obj, php_raylib_rectangle_ce);
+
+    php_raylib_rectangle_object *result = Z_RECTANGLE_OBJ_P(obj);
+    result->rectangle = GetCollisionRec(phpRec1->rectangle, phpRec2->rectangle);
+
+    result->rectangle;
+
+    RETURN_OBJ(&result->std);
+}
+
+
+
+// Check if point is inside circle
+// RLAPI bool CheckCollisionPointCircle(Vector2 point, Vector2 center, float radius);
+PHP_METHOD(Collision, checkPointCircle)
+{
+    zval *point;
+    zval *center;
+    double radius;
+
+    ZEND_PARSE_PARAMETERS_START(3, 3)
+        Z_PARAM_ZVAL(point)
+        Z_PARAM_ZVAL(center)
+        Z_PARAM_DOUBLE(radius)
+    ZEND_PARSE_PARAMETERS_END();
+
+    php_raylib_vector2_object *phpPoint = Z_VECTOR2_OBJ_P(point);
+    php_raylib_vector2_object *phpCenter = Z_VECTOR2_OBJ_P(center);
+
+    RETURN_BOOL(CheckCollisionPointCircle(phpPoint->vector2, phpCenter->vector2, (float) radius));
+}
+
+// Check if point is inside a triangle
+// RLAPI bool CheckCollisionPointTriangle(Vector2 point, Vector2 p1, Vector2 p2, Vector2 p3);
+PHP_METHOD(Collision, checkPointTriangle)
+{
+    zval *point;
+    zval *p1;
+    zval *p2;
+    zval *p3;
+
+    ZEND_PARSE_PARAMETERS_START(4, 4)
+        Z_PARAM_ZVAL(point)
+        Z_PARAM_ZVAL(p1)
+        Z_PARAM_ZVAL(p2)
+        Z_PARAM_ZVAL(p3)
+    ZEND_PARSE_PARAMETERS_END();
+
+    php_raylib_vector2_object *phpPoint = Z_VECTOR2_OBJ_P(point);
+    php_raylib_vector2_object *phpP1 = Z_VECTOR2_OBJ_P(p1);
+    php_raylib_vector2_object *phpP2 = Z_VECTOR2_OBJ_P(p2);
+    php_raylib_vector2_object *phpP3 = Z_VECTOR2_OBJ_P(p3);
+
+    RETURN_BOOL(CheckCollisionPointTriangle(phpPoint->vector2, phpP1->vector2, phpP2->vector2, phpP3->vector2));
+}
+
 
 const zend_function_entry php_raylib_collision_methods[] = {
         PHP_ME(Collision, __construct, NULL, ZEND_ACC_PUBLIC)
+        PHP_ME(Collision, checkRecs, NULL, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
+        PHP_ME(Collision, checkCircles, NULL, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
+        PHP_ME(Collision, checkCircleRec, NULL, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
+        PHP_ME(Collision, getRec, NULL, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
         PHP_ME(Collision, checkPointRec, NULL, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
+        PHP_ME(Collision, checkPointCircle, NULL, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
+        PHP_ME(Collision, checkPointTriangle, NULL, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC)
         PHP_FE_END
 };
 
