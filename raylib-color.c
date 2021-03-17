39241d218b4196c9d8ef153f8944c7acd1e76a7d
Joseph Montanez <sutabi@gmail.com>
2021-03-16 21:32:20 -0700

    code cleanup

--- raylib-color.c
+++ raylib-color.c
@@ -103,166 +103,111 @@ static zval *php_raylib_color_property_reader(php_raylib_color_object *obj, rayl
 }
 /* }}} */
 
-static zval *php_raylib_color_get_property_ptr_ptr(zval *object, zval *member, int type, void **cache_slot) /* {{{ */
+static zval *php_raylib_color_get_property_ptr_ptr(zend_object *object, zend_string *name, int type, void **cache_slot) /* {{{ */
 {
     php_raylib_color_object *obj;
-    zval tmp_member;
     zval *retval = NULL;
     raylib_color_prop_handler *hnd = NULL;
-    const zend_object_handlers *std_hnd;
 
-    if (Z_TYPE_P(member) != IS_STRING) {
-        ZVAL_COPY(&tmp_member, member);
-        convert_to_string(&tmp_member);
-        member = &tmp_member;
-        cache_slot = NULL;
-    }
-
-    obj = Z_COLOR_OBJ_P(object);
+    obj = php_raylib_color_fetch_object(object);
 
     if (obj->prop_handler != NULL) {
-        hnd = zend_hash_find_ptr(obj->prop_handler, Z_STR_P(member));
+        hnd = zend_hash_find_ptr(obj->prop_handler, name);
     }
 
     if (hnd == NULL) {
-        std_hnd = zend_get_std_object_handlers();
-        retval = std_hnd->get_property_ptr_ptr(object, member, type, cache_slot);
-    }
-
-    if (member == &tmp_member) {
-        zval_dtor(member);
+        retval = zend_std_get_property_ptr_ptr(object, name, type, cache_slot);
     }
 
     return retval;
 }
 /* }}} */
 
-static zval *php_raylib_color_read_property(zval *object, zval *member, int type, void **cache_slot, zval *rv) /* {{{ */
+static zval *php_raylib_color_read_property(zend_object *object, zend_string *name, int type, void **cache_slot, zval *rv) /* {{{ */
 {
     php_raylib_color_object *obj;
-    zval tmp_member;
     zval *retval = NULL;
     raylib_color_prop_handler *hnd = NULL;
-    const zend_object_handlers *std_hnd;
 
-    if (Z_TYPE_P(member) != IS_STRING) {
-        ZVAL_COPY(&tmp_member, member);
-        convert_to_string(&tmp_member);
-        member = &tmp_member;
-        cache_slot = NULL;
-    }
-
-    obj = Z_COLOR_OBJ_P(object);
+    obj = php_raylib_color_fetch_object(object);
 
     if (obj->prop_handler != NULL) {
-        hnd = zend_hash_find_ptr(obj->prop_handler, Z_STR_P(member));
-    } else {
-//        php_error_docref(NULL, E_WARNING, "Internal raylib color hnd not found");
+        hnd = zend_hash_find_ptr(obj->prop_handler, name);
     }
 
-    if (hnd != NULL) {
+    if (hnd) {
         retval = php_raylib_color_property_reader(obj, hnd, rv);
-        if (retval == NULL) {
-            php_error_docref(NULL, E_WARNING, "Internal raylib color retval is null");
-            retval = &EG(uninitialized_zval);
-        }
     } else {
-        std_hnd = zend_get_std_object_handlers();
-        retval = std_hnd->read_property(object, member, type, cache_slot, rv);
-    }
-
-    if (member == &tmp_member) {
-        zval_dtor(member);
+        retval = zend_std_read_property(object, name, type, cache_slot, rv);
     }
 
     return retval;
 }
 /* }}} */
 
-/* {{{ php_raylib_color_write_property(zval *object, zval *member, zval *value[, const zend_literal *key])
+/* {{{ php_raylib_color_write_property(zend_object *object, zend_string *member, zval *value, void **cache_slot)
    Generic object property writer */
-zval *php_raylib_color_write_property(zval *object, zval *member, zval *value, void **cache_slot)
+zval *php_raylib_color_write_property(zend_object *object, zend_string *member, zval *value, void **cache_slot)
 {
-    zval tmp_member;
     php_raylib_color_object *obj;
     raylib_color_prop_handler *hnd;
 
-    if (Z_TYPE_P(member) != IS_STRING) {
-        zend_string *str = zval_try_get_string_func(member);
-        if (UNEXPECTED(!str)) {
-            return value;
-        }
-        ZVAL_STR(&tmp_member, str);
-        member = &tmp_member;
+    obj = php_raylib_color_fetch_object(object);
+
+    if (obj->prop_handler != NULL) {
+        hnd = zend_hash_find_ptr(obj->prop_handler, member);
     }
 
-    obj = Z_COLOR_OBJ_P(object);
-
-    hnd = zend_hash_find_ptr(&php_raylib_color_prop_handlers, Z_STR_P(member));
-
-    if (hnd && hnd->write_float_func) {
+    if (hnd) {
         hnd->write_float_func(obj, value);
     } else {
         value = zend_std_write_property(object, member, value, cache_slot);
-    }
-
-    if (member == &tmp_member) {
-        zval_ptr_dtor(member);
     }
 
     return value;
 }
 /* }}} */
 
-static int php_raylib_color_has_property(zval *object, zval *member, int type, void **cache_slot) /* {{{ */
+static int php_raylib_color_has_property(zend_object *object, zend_string *name, int has_set_exists, void **cache_slot) /* {{{ */
 {
     php_raylib_color_object *obj;
-    zval tmp_member;
     raylib_color_prop_handler *hnd = NULL;
-    const zend_object_handlers *std_hnd;
-    int retval = 0;
+    int ret = 0;
 
-    if (Z_TYPE_P(member) != IS_STRING) {
-        ZVAL_COPY(&tmp_member, member);
-        convert_to_string(&tmp_member);
-        member = &tmp_member;
-        cache_slot = NULL;
-    }
-
-    obj = Z_COLOR_OBJ_P(object);
-
-    if (obj->prop_handler != NULL) {
-        hnd = zend_hash_find_ptr(obj->prop_handler, Z_STR_P(member));
-    }
-
-    if (hnd != NULL) {
-        zval tmp, *prop;
-
-        if (type == 2) {
-            retval = 1;
-        } else if ((prop = php_raylib_color_property_reader(obj, hnd, &tmp)) != NULL) {
-            if (type == 1) {
-                retval = zend_is_true(&tmp);
-            } else if (type == 0) {
-                retval = (Z_TYPE(tmp) != IS_NULL);
+    if ((hnd = zend_hash_find_ptr(obj->prop_handler, name)) != NULL) {
+        switch (has_set_exists) {
+            case ZEND_PROPERTY_EXISTS:
+                ret = 1;
+                break;
+            case ZEND_PROPERTY_NOT_EMPTY: {
+                zval rv;
+                zval *value = php_raylib_color_read_property(object, name, BP_VAR_IS, cache_slot, &rv);
+                if (value != &EG(uninitialized_zval)) {
+                    convert_to_boolean(value);
+                    ret = Z_TYPE_P(value) == IS_TRUE ? 1 : 0;
+                }
+                break;
             }
+            case ZEND_PROPERTY_ISSET: {
+                zval rv;
+                zval *value = php_raylib_color_read_property(object, name, BP_VAR_IS, cache_slot, &rv);
+                if (value != &EG(uninitialized_zval)) {
+                    ret = Z_TYPE_P(value) != IS_NULL? 1 : 0;
+                    zval_ptr_dtor(value);
+                }
+                break;
+            }
+                EMPTY_SWITCH_DEFAULT_CASE();
         }
-
-        zval_ptr_dtor(&tmp);
     } else {
-        std_hnd = zend_get_std_object_handlers();
-        retval = std_hnd->has_property(object, member, type, cache_slot);
+        ret = zend_std_has_property(object, name, has_set_exists, cache_slot);
     }
 
-    if (member == &tmp_member) {
-        zval_dtor(member);
-    }
-
-    return retval;
+    return ret;
 }
 /* }}} */
 
-static HashTable *php_raylib_color_get_gc(zval *object, zval **gc_data, int *gc_data_count) /* {{{ */
+static HashTable *php_raylib_color_get_gc(zend_object *object, zval **gc_data, int *gc_data_count) /* {{{ */
 {
     *gc_data = NULL;
     *gc_data_count = 0;
@@ -270,14 +215,14 @@ static HashTable *php_raylib_color_get_gc(zval *object, zval **gc_data, int *gc_
 }
 /* }}} */
 
-static HashTable *php_raylib_color_get_properties(zval *object)/* {{{ */
+static HashTable *php_raylib_color_get_properties(zend_object *object)/* {{{ */
 {
     php_raylib_color_object *obj;
     HashTable *props;
     raylib_color_prop_handler *hnd;
     zend_string *key;
 
-    obj = Z_COLOR_OBJ_P(object);
+    obj = php_raylib_color_fetch_object(object);
     props = zend_std_get_properties(object);
 
     if (obj->prop_handler == NULL) {
