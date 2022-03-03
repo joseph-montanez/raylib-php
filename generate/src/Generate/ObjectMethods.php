c615e46a55bffa9c44d5d19f6ec343d188b738eb
Joseph Montanez <sutabi@gmail.com>
2022-03-02 21:39:27 -0800

    added constructor

--- generate/src/Generate/ObjectMethods.php
+++ generate/src/Generate/ObjectMethods.php
@@ -12,7 +12,7 @@ class ObjectMethods
     public function generate(Struct $struct, array $input): array
     {
         $input [] = 'const zend_function_entry php_raylib_' . $struct->nameLower . '_methods[] = {';
-//        $input [] = '        PHP_ME(' . $struct->nameLower . ', __construct, arginfo_' . $struct->nameLower . '__construct, ZEND_ACC_PUBLIC)';
+        $input [] = '        PHP_ME(' . $struct->name . ', __construct, arginfo_' . $struct->nameLower . '__construct, ZEND_ACC_PUBLIC)';
 //        $input [] = '        PHP_ME(' . $struct->nameLower . ', getX, arginfo_' . $struct->nameLower . '_getX, ZEND_ACC_PUBLIC)';
 //        $input [] = '        PHP_ME(' . $struct->nameLower . ', setX, arginfo_' . $struct->nameLower . '_setX, ZEND_ACC_PUBLIC)';
 //        $input [] = '        PHP_ME(' . $struct->nameLower . ', getY, arginfo_' . $struct->nameLower . '_getY, ZEND_ACC_PUBLIC)';
