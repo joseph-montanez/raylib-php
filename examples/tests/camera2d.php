c5aaebadc0500b5d0292fcaf30fd67f59bd15501
Joseph Montanez <sutabi@gmail.com>
2023-08-01 18:18:53 -0700

    release prep

--- examples/tests/camera2d.php
+++
@@ -1,7 +0,0 @@
-<?php
-$camera = new \raylib\Camera2d();
-
-$camera->target->x = 5;
-$camera->target->y = 6;
-
-var_dump($camera->target);
--- examples/tests/image-pixels.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$image = new \raylib\Image(__DIR__ . '/../textures/resources/raylib_logo.png');
-$pixelColors = $image->getData();
-var_dump(count($pixelColors));
-
-foreach ($pixelColors as $color) {
-    echo implode(', ', [$color->r, $color->g, $color->b, $color->a]), PHP_EOL;
-}
